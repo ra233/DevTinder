0924419e42d00a599fcfafb4d8339e121fde8205
ra233 <54186082+ra233@users.noreply.github.com>
2024-12-17 12:56:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Namaste Akshay!");
+  res.send("Namaste server!");
 });
 
 app.use("/hello", (req, res) => {
