312cda0ab9ec7a73b7f678a456b06d92cca049a3
unknown <ranjitha.kn03@gmail.com>
2024-12-17 13:40:40 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,25 +1,40 @@
-const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Ranjitha", lastName: "N" });
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "ran", lastName: "n" });
 });
+//app.use("/route", rH, [rH2, rH3], rH4, rh5);
 
-app.post("/user", async (req, res) => {
-  console.log(req.body);
-  // saving data to DB
-  res.send("Data successfully saved to the database!");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    // res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!");
+    // res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5!!");
+    res.send("5th Response!!");
+  }
+);
 
 app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777...");
+console.log("Server is successfully listening on port 7777...")
 });
