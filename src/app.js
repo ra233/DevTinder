3c9d5d11199993d69db8dea07d3e98b8bf77d21f
unknown <ranjitha.kn03@gmail.com>
2024-12-17 13:50:10 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,40 +1,21 @@
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "ran", lastName: "n" });
+const { adminAuth, userAuth } = require("./middlewares/auth");
+app.use("/admin", adminAuth);
+app.post("/user/login", (req, res) => {
+  res.send("User logged in successfully!");
 });
-//app.use("/route", rH, [rH2, rH3], rH4, rh5);
-
-app.get(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the route user!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2!!");
-    // res.send("2nd Response!!");
-    next();
-  },
-
-  (req, res, next) => {
-    console.log("Handling the route user 3!!");
-    // res.send("3rd Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4!!");
-    // res.send("4th Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 5!!");
-    res.send("5th Response!!");
-  }
-);
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent");
+});
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
 
 app.listen(7777, () => {
-console.log("Server is successfully listening on port 7777...")
+  console.log("Server is successfully listening on port 7777...")
 });
